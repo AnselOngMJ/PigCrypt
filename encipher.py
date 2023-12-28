d6ee6f5ebf4900d01d6c5bffa7c4568f8cba7f80
AnselOngMJ <111511997+AnselOngMJ@users.noreply.github.com>
2023-12-28 23:38:56 +0000

    Rename variables and move constants to top of file

--- encipher.py
+++ encipher.py
@@ -1,54 +1,59 @@
 from PIL import Image, ImageDraw
 
 
+LINES = {
+    "top": list(range(3, 9)) + list(range(12, 18)),
+    "right": [0, 1, 3, 4, 6, 7, 9, 10, 12, 13, 15, 16],
+    "bottom": list(range(6)) + list(range(9, 15)),
+    "left": [1, 2, 4, 5, 7, 8, 10, 11, 13, 14, 16, 17],
+}
+ARROWS = {
+    "top": [21, 25],
+    "right": [19, 23],
+    "bottom": [18, 22],
+    "left": [20, 24],
+}
+DOTS = list(range(9, 18)) + list(range(22, 26))
+ALPHABET = [chr(i) for i in range(97, 123)]
+
+
 def main():
     im = create_ciphertext("hello world. i am ansel.")
     im.save("ciphertext.png")
 
-def create_ciphertext(text):
-    lines = [line.strip() for line in text.split(".")]
+
+def create_ciphertext(plaintext):
+    lines = [line.strip() for line in plaintext.split(".")]
     if "" in lines:
         lines.remove("")
     size = max([len(line) for line in lines]) * 32, len(lines) * 32
     im = Image.new("1", size, 1)
     for y, line in enumerate(lines):
-        for x, c in enumerate(line):
-            im.paste(draw_character(c), (x * 32, y * 32))
+        for x, character in enumerate(line):
+            im.paste(draw_character(character), (x * 32, y * 32))
     return im
 
-def draw_character(c):
+
+def draw_character(character):
     size = 32, 32
     im = Image.new("1", size, 1)
     draw = ImageDraw.Draw(im)
-    letters = [chr(i) for i in range(97, 123)]
-    lines = {
-        "top_line": list(range(3, 9)) + list(range(12, 18)),
-        "right_line": [0, 1, 3, 4, 6, 7, 9, 10, 12, 13, 15, 16],
-        "bottom_line": list(range(6)) + list(range(9, 15)),
-        "left_line": [1, 2, 4, 5, 7, 8, 10, 11, 13, 14, 16, 17],
-    }
-    arrows = {
-        "top_arrow": [21, 25],
-        "right_arrow": [19, 23],
-        "bottom_arrow": [18, 22],
-        "left_arrow": [20, 24],
-    }
-    dot = list(range(9, 18)) + list(range(22, 26))
-    if c == " ":
+    if character == " ":
         return im
-    letter = letters.index(c)
+    letter = ALPHABET.index(character)
     if letter < 18:
-        for key, value in lines.items():
+        for key, value in LINES.items():
             if letter in value:
-                draw_line(draw, key.removesuffix("_line"))
+                draw_line(draw, key)
     else:
-        for key, value in arrows.items():
+        for key, value in ARROWS.items():
             if letter in value:
-                draw_arrows(draw, key.removesuffix("_arrow"))
+                draw_arrows(draw, key)
                 break
-    if letter in dot:
+    if letter in DOTS:
         draw_dot(draw)
     return im
+
 
 def draw_line(draw, position):
     match position:
@@ -60,6 +65,7 @@ def draw_line(draw, position):
             draw.line((3, 27, 28, 27), width=3)
         case "left":
             draw.line((4, 3, 4, 28), width=3)
+
 
 def draw_arrows(draw, direction):
     match direction:
@@ -76,8 +82,10 @@ def draw_arrows(draw, direction):
             draw.line((3, 15, 27, 3), width=3)
             draw.line((3, 16, 27, 28), width=3)
 
+
 def draw_dot(draw):
     draw.ellipse((13, 13, 18, 18), fill=0)
+
 
 if __name__ == "__main__":
     main()
